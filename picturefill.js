28db0e6deddb572721119b183dbdf06f969517b0
edgars-sirokovs <edgars.sirokovs@gmail.com>
2014-04-11 12:29:33 +0300

    Update picturefill.js
    Modified to simplify markup and remove unnecessary DOM elements

--- picturefill.js
+++ picturefill.js
@@ -1,4 +1,6 @@
-/*! Picturefill - Responsive Images that work today. (and mimic the proposed Picture element with span elements). Author: Scott Jehl, Filament Group, 2012 | License: MIT/GPLv2 */
+
+/*! Picturefill - Responsive Images that work today. (and mimic the proposed Picture element with span elements). Author: Scott Jehl, Filament Group, 2012 | License: MIT/GPLv2 
+modified specially for Albumeria */
 
 (function( w ){
 
@@ -12,45 +14,61 @@
 		for( var i = 0, il = ps.length; i < il; i++ ){
 			if( ps[ i ].getAttribute( "data-picture" ) !== null ){
 
-				var sources = ps[ i ].getElementsByTagName( "span" ),
-					matches = [];
+			var baseurl = ps[ i ].getAttribute( "data-baseurl" );
+			var queries = ps[ i ].getAttribute( "data-queries" );
 
-				// See if which sources match
-				for( var j = 0, jl = sources.length; j < jl; j++ ){
-					var media = sources[ j ].getAttribute( "data-media" );
-					// if there's no media specified, OR w.matchMedia is supported 
+			var matches = [];
+			var query = "", media = "", src = "";
+			var src = "";
+			var media = "";
+
+			if (queries) {
+				queries = queries.split(",");
+
+				for (var q in queries) {
+					query = queries[q].split(":");
+
+					media = (query[0] === "default") ? "" : "(max-width:"+query[0]+ "px)";
+
 					if( !media || ( w.matchMedia && w.matchMedia( media ).matches ) ){
-						matches.push( sources[ j ] );
-					}
-				}
-
-				// Find any existing img element in the picture element
-				var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
-
-				if( matches.length ){
-					var matchedEl = matches.pop();
-					if( !picImg || picImg.parentNode.nodeName === "NOSCRIPT" ){
-						picImg = w.document.createElement( "img" );
-						var alt = ps[ i ].getAttribute( "data-alt" );
-						if (alt !== null) {
-							picImg.alt = alt;
-						}
-					}
-					else if( matchedEl === picImg.parentNode ){
-						// Skip further actions if the correct image is already in place
-						continue;
+						src = baseurl+"?"+query[1];
 					}
 
-					picImg.src =  matchedEl.getAttribute( "data-src" );
-					matchedEl.appendChild( picImg );
-					picImg.removeAttribute("width");
-					picImg.removeAttribute("height");
-				}
-				else if( picImg ){
-					picImg.parentNode.removeChild( picImg );
+					media = (query[0] === "default") ? "(-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi), (min-resolution: 2dppx)" : "(max-width:"+query[0]+ "px) and (-webkit-min-device-pixel-ratio: 2)" + ", (max-width:"+query[0]+ "px) and (min-resolution: 192dpi)" + ", (max-width:"+query[0]+ "px) and (min-resolution: 2dppx)";
+
+					if( !media || ( w.matchMedia && w.matchMedia( media ).matches ) ){
+						src = baseurl+"?"+query[1]+"&dppx=2";
+					}
+
 				}
 			}
+
+
+			// Find any existing img element in the picture element
+			var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
+
+			if( src != "" ){
+				if( !picImg || picImg.parentNode.nodeName === "NOSCRIPT" ){
+					picImg = w.document.createElement( "img" );
+					picImg.alt = ps[ i ].getAttribute( "data-alt" );
+				}
+				else if( src === picImg.getAttribute("src") ){
+					// Skip further actions if the correct image is already in place
+					continue;
+				}
+
+				picImg.src =  src;
+				ps[i].appendChild( picImg );
+				picImg.removeAttribute("width");
+				picImg.removeAttribute("height");
+			}
+			else if( picImg ){
+				picImg.parentNode.removeChild( picImg );
+			}
+
 		}
+		}
+	
 	};
 
 	// Run on resize and domready (w.load as a fallback)
